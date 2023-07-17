4f4470c76ad87df931a8058c8ff593bfa67fe20f
Damageddream <marcan.grab@gmail.com>
2023-07-17 18:30:20 +0200

    working on strategy

--- src/middleware/authentication.ts
+++ src/middleware/authentication.ts
@@ -1,10 +1,29 @@
 import passport from "passport";
 import { Strategy as FacebookStrategy } from "passport-facebook";
+import { Request, Response, NextFunction } from "express";
 
-passport.serializeUser(function (user, done) {
-  done(null, user);
-});
 
-passport.deserializeUser(function (user, done) {
-  done(null, user);
-});
+
+
+const passportFacebook = (req: Request, res: Response, next: NextFunction) => {
+  passport.serializeUser(function (user, done) {
+    done(null, user);
+  });
+
+  passport.deserializeUser(function (user, done) {
+    done(null, user);
+  });
+
+  passport.use(
+    new FacebookStrategy(
+      {
+        clientID: process.env.FB_ID as string,
+        clientSecret: process.env.FB_SECRET as string,
+        callbackURL: "http://localhost:8000/auth/callback",
+      },
+      function (accessToken, refreshToken, profile, done) {
+        return done(null, profile);
+      }
+    )
+  );
+};
